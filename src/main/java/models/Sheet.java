09ab66768775f9709618413a71ba3667bfcc4cb3
olenalo <uleta@ukr.net>
2018-12-28 05:34:54 +0200

    Remove redundant code

--- src/main/java/models/Sheet.java
+++ src/main/java/models/Sheet.java
@@ -1,7 +1,5 @@
 package models;
 
-import java.util.*;
-
 import static configs.SpreadsheetConfigs.DEFAULT_SHEET_NAME;
 import static configs.SpreadsheetConfigs.DEFAULT_ROWS_NUMBER;
 import static configs.SpreadsheetConfigs.DEFAULT_COLUMNS_NUMBER;
@@ -12,14 +10,12 @@ public class Sheet {
     private String title;
     private Long rowsNumber;
     private Long columnsNumber;
-    private Map<Location, String> entries;
 
     /**
      * Initialize a sheet with a default entries.
      */
     public Sheet(Long id) {
         this.id = id;
-        this.entries = new HashMap<>();
         this.initializeDefaultSize();
         this.title = DEFAULT_SHEET_NAME; // TODO make it unique for each sheet
     }
@@ -82,51 +78,6 @@ public class Sheet {
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    /**
-     * Get a slice of the sheet data.
-     *
-     * @param from
-     * @param to
-     */
-    public void getEntries(Location from, Location to) {
-        throw new UnsupportedOperationException("This method isn't implemented yet");
-    }
-
-    // TODO make it possible to add values of different types
-
-    /**
-     * Add a single value to a cell.
-     *
-     * @param location
-     * @param value
-     */
-    public void addData(Location location, String value) {
-        // TODO check location (no data can be added to non-existing cell)
-        entries.put(location, value);
-    }
-
-    /**
-     * Add data to multiple cells.
-     *
-     * @param from
-     * @param to
-     * @param values
-     */
-    public void addData(Location from, Location to, List<String> values) {
-        // TODO check location (no data can be added to non-existing cell)
-        // TODO fill in the data; non-existent values to become `null`?
-    }
-
-    /**
-     * Add data to multiple cells.
-     *
-     * @param data
-     */
-    public void addData(Map<Location, String> data) {
-        // TODO check location (no data can be added to non-existing cell)
-        // TODO fill in the data
     }
 
     @Override
